fed8eccfc03fda5095ac1e9ae947db601b1e8784
Glitch (sydoracle-sendmail) <none>
2018-03-04 03:43:19 +0000

    Use latest packages from SendGrid

--- server.js
+++ server.js
@@ -5,37 +5,28 @@
 // init project
 var express = require('express');
 var app = express();
-var helper = require('sendgrid').mail;
-var fromEmail = new helper.Email('glitchsm@sydoracle.com');
-var toEmail = new helper.Email('glitchtest@sydoracle.com');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'Hello, Email!');
-var mail = new helper.Mail(fromEmail, subject, toEmail, content);
 
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON()
-});
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-if(process.env.SENDGRID_API_KEY){
-  console.log('Sending');
-  //sg.API(request, function (error, response) {
-  //  if (error) {
-  //    console.log('Error response received');
-  //  }
-  //  console.log(response.statusCode);
-  //  console.log(response.body);
-  //  console.log(response.headers);
-  //});
-}
+const msg = {
+  to: 'glitch_rcpt@sydoracle.com',
+  from: 'glitch_send@sydoracle.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/bumble", function (request, response) {
+  sgMail.send(msg);
   response.sendFile(__dirname + '/views/index.html');
 });
 
